5feac89bae932d16fe96b69baad84d082dd95b40
SungKyu Lim <lmh7249@naver.com>
2025-01-08 16:19:14 +0900

    first commit

---
+++ src/test/java/com/hamtalk/HamtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamtalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

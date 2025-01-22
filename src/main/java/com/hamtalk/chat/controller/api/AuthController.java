e763e97d4fc254b3e4506b073e06f8490aded176
SungKyu Lim <lmh7249@naver.com>
2025-01-22 15:57:04 +0900

    test: test

---
+++ src/main/java/com/hamtalk/chat/controller/api/AuthController.java
@@ -0,0 +1,17 @@
+package com.hamtalk.chat.controller.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+
+
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login() {
+//
+//    }
+
+}
--- src/main/java/com/hamtalk/chat/controller/api/UserController.java
+++ src/main/java/com/hamtalk/chat/controller/api/UserController.java
@@ -1,23 +1,22 @@
 package com.hamtalk.chat.controller.api;
 
-import com.hamtalk.chat.model.request.UserSignupRequest;
 import com.hamtalk.chat.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<String> signup(UserSignupRequest dto) {
-        String message = userService.signup(dto);
-        return ResponseEntity.ok(message);
-    }
+//    @PostMapping
+//    public ResponseEntity<String> signup(UserSignupRequest dto) {
+//        String message = userService.signup(dto);
+//        return ResponseEntity.ok(message);
+//    }
+
+
 
 }

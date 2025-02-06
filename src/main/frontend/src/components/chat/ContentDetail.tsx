183267dae694615631b2d1f9eada8da0cd84c011
SungKyu Lim <lmh7249@naver.com>
2025-02-06 18:28:17 +0900

    feat: EmptyContentDetail 페이지 제작

--- src/main/frontend/src/components/chat/ContentDetail.tsx
+++ src/main/frontend/src/components/chat/ContentDetail.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
+import EmptyContentDetail from "./EmptyContentDetail";
 
 const StyledContentDetail = styled.div`
     border: solid 1px black;
     flex-grow: 1;
     background-color: #F5F6F7;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     
 `
 
 const ContentDetail = () => {
     return (
         <StyledContentDetail>
-            중앙 우측 메인 메뉴2
+            <EmptyContentDetail/>
             {/*(메뉴1에 있는 내용, 채팅창 클릭 등을하면 여기서 화면을 보여줌, 사용자는 주로 여기서*/}
             {/*채팅을 치거나 수정, 다른 유저의 프로필을 보거나 등등.*/}
         </StyledContentDetail>
--- src/main/frontend/src/components/chat/ContentList.tsx
+++ src/main/frontend/src/components/chat/ContentList.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import FriendPlusIcon from "../../assets/icons/friend-plus.svg";
+import ChattingRoomPlusIcon from "../../assets/icons/chatting-room-plus.svg";
 import FriendList from "../friends/FriendList";
 import SearchIcon from "../../assets/icons/search.svg"
 import ChattingRoomList from "../chattingroom/ChattingRoomList";
@@ -25,11 +26,11 @@ const ContentListTopState = ({selectedMenu}:MenuState) => {
     return (
         <StyledContentListTopState>
             <h3>{selectedMenu.label}</h3>
-            <img src={FriendPlusIcon} alt="친구 추가" width={30} height={30}/>
+            {selectedMenu.key === "friends" && <img src={FriendPlusIcon} alt="친구 추가" width={30} height={30}/>}
+            {selectedMenu.key === "chats" && <img src={ChattingRoomPlusIcon} alt="채팅방 생성" width={30} height={30}/>}
         </StyledContentListTopState>
     )
 }
-
 
 const SearchInput = styled.input`
     background-color: #DFDFDF;
@@ -47,10 +48,11 @@ const ContentList = () => {
 
     return (
         <StyledContentList>
-
             <ContentListTopState selectedMenu={selectedMenu}/>
-            <SearchInput type="text" placeholder="이름 또는 이메일을 입력하세요."></SearchInput>
-            <div> 친구 200</div>
+            {selectedMenu.key === "friends" && <SearchInput type="text" placeholder="이름 또는 이메일을 입력하세요."/>}
+            {selectedMenu.key === "chats" && <SearchInput type="text" placeholder="참여자 또는 채팅방명을 검색하세요."/>}
+            {selectedMenu.key === "friends" && <div> 친구 200</div>}
+            {selectedMenu.key === "chats" && <div> 채팅방 200</div>}
             {selectedMenu.key === "friends" && <FriendList/>}
             {selectedMenu.key === "chats" && <ChattingRoomList/>}
             {/*{selectedMenu === "settings" && <ChattingRoomList/>}*/}

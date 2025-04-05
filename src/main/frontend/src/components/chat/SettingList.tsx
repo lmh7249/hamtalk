972064eeea8e14f428279377189b93c9ced43a5f
SungKyu Lim <lmh7249@naver.com>
2025-04-05 22:51:54 +0900

    feat: 내 프로필 보기 기능 구현

--- src/main/frontend/src/components/chat/SettingList.tsx
+++ src/main/frontend/src/components/chat/SettingList.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import React from "react";
+import {setUserProfile} from "../../store/contentDetailSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../store";
+import toast from "react-hot-toast";
 
-const SettingList = () => {
-    const ProfileButton = styled.button`
+const ProfileButton = styled.button`
         background-color: #f5f5f5;
         border: none;
         padding: 10px 20px;
@@ -18,8 +22,26 @@ const SettingList = () => {
     `;
 
 
+
+
+
+
+const SettingList = () => {
+    const dispatch = useDispatch();
+    const myUserId = useSelector((state:RootState) => state.user.id);
+
+    const handleClick = (userId: number | null) => {
+        if(userId === null) {
+            toast.error("내 프로필 불러올 수 없어요. 로그아웃 후, 다시 이용 해주세요.");
+            return;
+        }
+        dispatch(setUserProfile({userId: userId}));
+    }
+
+
+
     return (
-        <ProfileButton>내 프로필 보기</ProfileButton>
+        <ProfileButton onClick={() => handleClick(myUserId)}>내 프로필 보기</ProfileButton>
 
     )
 }

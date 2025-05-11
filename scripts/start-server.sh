8ff870df1581a7f28995bfba0571bd36117c6071
SungKyu Lim <lmh7249@naver.com>
2025-05-11 17:46:05 +0900

    fix: ECR 주소 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,6 +4,6 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop hamtalk-server || true
 docker rm hamtalk-server || true
-docker pull 235494776530.dkr.ecr.ap-northeast-2.amazonaws.com/hamtalk-server/hamtalk-server:latest
-docker run -d --name hamtalk-server -p 8080:8080 235494776530.dkr.ecr.ap-northeast-2.amazonaws.com/hamtalk-server/hamtalk-server:latest
+docker pull 235494776530.dkr.ecr.ap-northeast-2.amazonaws.com/hamtalk-server:latest
+docker run -d --name hamtalk-server -p 8080:8080 235494776530.dkr.ecr.ap-northeast-2.amazonaws.com/hamtalk-server:latest
 echo "--------------- 서버 배포 끝 -----------------"

4fd25134495410f772b28193176a250cdbc33754
SungKyu Lim <lmh7249@naver.com>
2025-06-28 16:29:15 +0900

    fix: 친구 목록에서 채팅방 접속 시, 참여자 목록에서 본인을 못불러오는 버그 수정

--- src/main/frontend/src/components/chat/FriendItem.tsx
+++ src/main/frontend/src/components/chat/FriendItem.tsx
@@ -36,7 +36,7 @@ const ImageWrapper = styled.div`
     border-radius: 50%;
     width: 60px;
     height: 60px;
-    overflow: hidden; 
+    overflow: hidden;
     box-sizing: border-box;
 `;
 
@@ -52,42 +52,51 @@ const FriendItem = ({userId}: FriendProfileProps) => {
     const {data: friends = [], isLoading, error} = useMyFriendsQuery(isFriendsTab);
     //TODO: Map으로 미리 변환해두면 더 빠름 (예: id -> friend)
     const friend = friends.find(friend => friend.toUserId === userId);
+    const myProfile = useSelector((state: RootState) => state.user);
+
     if (!friend) return null;
 
     const handleProfileDoubleClick = async () => {
-        //TODO: profileImageUrl 함께 반환하기.
+        if (!myProfile.id) {
+            // 내 ID가 null이거나 유효하지 않으면 아무 작업도 하지 않고 함수를 종료
+            console.error("사용자 정보가 올바르지 않아 채팅방을 열 수 없습니다.");
+            return;
+        }
+        const participantsPayload = [
+            { // 내 정보
+                userId: myProfile.id,
+                nickname: myProfile.nickname ?? '이름없음',
+                profileImageUrl: myProfile.profileImageUrl,
+            },
+            { // 친구 정보
+                userId: friend.toUserId,
+                nickname: friend.nickname ?? '알 수 없는 친구',
+                profileImageUrl: friend.profileImageUrl,
+            }
+        ];
         const response = await findDirectChatRoom(userId);
-
+        let chatRoomToSet: CurrentChatRoom;
         if (response === undefined || response === null) {
             dispatch(openChatRoom(null));
-            const currentChatRoom: CurrentChatRoom = {
+            chatRoomToSet = {
                 chatRoomId: null,
                 chatRoomName: friend.nickname,
                 creatorId: null,
-                participants: [{
-                    userId: friend.toUserId,
-                    nickname: friend.nickname,
-                    profileImageUrl: friend.profileImageUrl,
-                }],
+                participants: participantsPayload,
                 chatRoomImageUrl: friend.profileImageUrl,
-            }
-            dispatch(setCurrentChatRoom(currentChatRoom));
-            return;
+            };
+        } else {
+            dispatch(openChatRoom(response.chatRoomId));
+            //TODO: 여기도 dispatch로 이동할 채팅방 데이터 세팅하기.
+            chatRoomToSet = {
+                chatRoomId: response.chatRoomId,
+                chatRoomName: response.chatRoomName,
+                creatorId: response.creatorId,
+                participants: participantsPayload,
+                chatRoomImageUrl: friend.profileImageUrl,
+            };
         }
-        dispatch(openChatRoom(response.chatRoomId));
-        //TODO: 여기도 dispatch로 이동할 채팅방 데이터 세팅하기.
-        const currentChatRoom: CurrentChatRoom = {
-            chatRoomId: response.chatRoomId,
-            chatRoomName: response.chatRoomName,
-            creatorId: response.creatorId,
-            participants: [{
-                userId: friend.toUserId,
-                nickname: friend.nickname,
-                profileImageUrl: friend.profileImageUrl,
-            }],
-            chatRoomImageUrl: friend.profileImageUrl,
-        }
-        dispatch(setCurrentChatRoom(currentChatRoom));
+        dispatch(setCurrentChatRoom(chatRoomToSet));
     }
 
     const handleImageClick = (e: React.MouseEvent, userId: number) => {
@@ -100,7 +109,8 @@ const FriendItem = ({userId}: FriendProfileProps) => {
             <ImageWrapper onClick={(e: React.MouseEvent) => handleImageClick(e, userId)}>
                 <StyledImage src={friend.profileImageUrl} alt="유저이미지"/>
             </ImageWrapper>
-            <UserInfoText nickName={friend.nickname} statusMessage={friend.statusMessage} email={friend.email} $isMe={false} statusLength={23}/>
+            <UserInfoText nickName={friend.nickname} statusMessage={friend.statusMessage} email={friend.email}
+                          $isMe={false} statusLength={23}/>
         </StyledFriendProfile>
     )
 }
